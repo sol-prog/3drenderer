14b0fa7a135e099d4c3e7a50531469c07b9c9a4f
Paul <solarianprogrammer.com>
2024-10-03 23:39:31 -0400

    remove unnecessary clear display instruction

--- src/main.c
+++ src/main.c
@@ -5,13 +5,13 @@
 
 void draw(ColorBuffer *color_buffer) {
     ClearColorBuffer(color_buffer, 0xFF000000);
-    //draw_grid(color_buffer, 20, 0xFF0000FF);
-    //test_draw_rectangles(color_buffer);
+    // draw_grid(color_buffer, 20, 0xFF0000FF);
+    // test_draw_rectangles(color_buffer);
     test_draw_pixels(color_buffer);
 }
 
 void update(void) {
-    DisplayDelay(500);
+    DisplayDelay(100);
 }
 
 int main(void) {
@@ -36,7 +36,7 @@ int main(void) {
     while(is_running) {
         DisplayProcessInput(&is_running);
         update();
-        ClearDisplay(display, 255, 0, 0, 255);       
+        // ClearDisplay(display, 255, 0, 0, 255);
         draw(color_buffer);
         DisplayRenderBuffer(display, color_buffer->data);
         PresentDisplay(display);
