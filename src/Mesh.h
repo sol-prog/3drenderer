ac403d848116de0837012b09c20bf4f2d4834a1d
Paul <solarianprogrammer.com>
2024-10-06 23:22:44 -0400

    forgot to add the files for Mesh and Triangle

---
+++ src/Mesh.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Vector.h"
+#include "Triangle.h"
+
+#define N_MESH_VERTICES 8
+extern Vec3 mesh_vertices[N_MESH_VERTICES];
+
+#define N_MESH_FACES 12 // 6 x 2 cube faces
+extern Face mesh_faces[N_MESH_FACES];
---
+++ src/Triangle.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Vector.h"
+
+typedef struct {
+    int a;
+    int b;
+    int c;
+} Face;
+
+typedef struct {
+    Vec2 points[3];
+} Triangle;
