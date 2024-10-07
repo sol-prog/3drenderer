5d5ccc97cf7ecaf982567ec7442cb943a9c96dce
Paul <solarianprogrammer.com>
2024-10-06 23:21:44 -0400

    Draw the vertices of a cube - a bit messy code, but it works

--- src/main.c
+++ src/main.c
@@ -3,32 +3,23 @@
 #include "ColorBuffer.h"
 #include "Drawing.h"
 #include "Vector.h"
+#include "Mesh.h"
 
-enum {N_POINTS = 9 * 9 * 9};
+Triangle triangles_to_render[N_MESH_FACES];
+
 const float FOV_FACT = 640;
-Vec3 cube_points[N_POINTS];
-Vec2 proj_points[N_POINTS];
 Vec3 camera_pos = {0, 0, -5};
 Vec3 cube_rotation = {0, 0, 0};
 
 
 void setup(void) {
-    int curr_point = 0;
-    for(float x = -1; x <= 1; x+= 0.25) {
-        for(float y = -1; y <= 1; y+= 0.25) {
-            for(float z = -1; z <= 1; z+= 0.25) {
-                cube_points[curr_point] = (Vec3){x, y, z};
-                curr_point++; 
-            }
-        }
-    }
 }
 
 Vec2 project(Vec3 *pt3, float fov_factor) {
     return (Vec2){fov_factor * pt3->x/pt3->z, fov_factor *pt3->y/pt3->z};
 }
 
-void update(Display *display) {
+void update(Display *display, ColorBuffer *color_buffer) {
     // Fix the FPS
     DisplayDelayForFPS(display);
 
@@ -36,26 +27,47 @@ void update(Display *display) {
     cube_rotation.y += 0.01;
     cube_rotation.z += 0.01;
 
-    for(int i = 0; i < N_POINTS; ++i) {
-        Vec3 point = cube_points[i];
+    for(int i = 0; i < N_MESH_FACES; ++i) {
+        Face mesh_face = mesh_faces[i];
+        Vec3 face_vertices[3];
+        face_vertices[0] = mesh_vertices[mesh_face.a - 1];
+        face_vertices[1] = mesh_vertices[mesh_face.b - 1];
+        face_vertices[2] = mesh_vertices[mesh_face.c - 1];
 
-        // Rotation
-        point = vec3_rotate_x(point, cube_rotation.x);
-        point = vec3_rotate_y(point, cube_rotation.y);
-        point = vec3_rotate_z(point, cube_rotation.z);
+        for(int j = 0; j < 3; ++j) {
+            Vec3 transformed_vertex = face_vertices[j];
+            transformed_vertex = vec3_rotate_x(transformed_vertex, cube_rotation.x);
+            transformed_vertex = vec3_rotate_y(transformed_vertex, cube_rotation.y);
+            transformed_vertex = vec3_rotate_z(transformed_vertex, cube_rotation.z);
 
-        // Camera
-        point.z -= camera_pos.z;
+            // Translate the vertex away from the camera
+            transformed_vertex.z -= camera_pos.z;
 
-        // Projection
-        proj_points[i] = project(&point, FOV_FACT);
+            // Projection
+            Vec2 projected_vertex = project(&transformed_vertex, FOV_FACT);
+
+            // Scale, translate projected points to middle of screen
+            projected_vertex.x += + 0.5 * color_buffer->width;
+            projected_vertex.y += + 0.5 * color_buffer->height;
+
+            triangles_to_render[i].points[j] = projected_vertex;
+        }
     }
 }
 
 void draw(ColorBuffer *color_buffer) {
     ClearColorBuffer(color_buffer, 0xFF000000);
-    for(int i = 0; i < N_POINTS; ++i) {
-        draw_rect(color_buffer, proj_points[i].x + 0.5 * color_buffer->width, proj_points[i].y + 0.5 * color_buffer->height, 4, 4, 0xFFFFFF00);
+    for(int i = 0; i < N_MESH_FACES; ++i) {
+        for(int j = 0; j < 3; ++j) {
+            draw_rect(
+                color_buffer,
+                triangles_to_render[i].points[j].x,
+                triangles_to_render[i].points[j].y, 
+                4,
+                4,
+                0xFFFFFF00
+            );
+        }
     }
 }
 
@@ -83,7 +95,7 @@ int main(void) {
 
     while(is_running) {
         DisplayProcessInput(&is_running);
-        update(display);
+        update(display, color_buffer);
         draw(color_buffer);
         DisplayRenderBuffer(display, color_buffer->data);
         PresentDisplay(display);
